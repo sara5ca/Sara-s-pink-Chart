1c9a4765670573e72fb1179cc0ea00aeafea94b1
sara5ca <45744150+sara5ca@users.noreply.github.com>
2020-01-19 15:04:59 -0500

    same

---
+++ pinkchart.py
@@ -0,0 +1,21 @@
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+x1=-5
+x2=15
+y1=-15
+y2=-5
+plt.axis([x1,x2,y1,y2])
+plt.axis('on')
+
+
+dx=.5
+dy=.5
+for x in np.arange(x1,x2,dx):
+ for y in np.arange(y1,y2,dy):
+    plt.scatter(x,y,s=1,color='fuchsia')
+    
+plt.grid(True,color='fuchsia')
+plt.title('Sara s pink Chart')
